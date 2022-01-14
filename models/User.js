d460561a7cae3d589a4ae9580ae17dec00924dde
Stephen Puthenpurackal <datfoosteve@gmail.com>
2022-01-14 17:03:19 -0600

    userSchema

--- models/User.js
+++ models/User.js
@@ -1,3 +1,45 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, SchemaTypeOptions } = require("mongoose");
+const thoughtSchema = require("./Thought");
+const userSchema = require("./User");
 
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      //mongoose match
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: thoughtSchema,
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: userSchema,
+      },
+    ]
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtual: true
+    },
+  }
+);
 
+userSchema.virtual("friendCount").get(function () {
+  return `${this.friends.length}`;
+});
+
+const User = model("user", UserSchema);
+module.exports = User;
